1552f16fa473ef31573c760e7f15000bd2be07cf
Louixie <101340214+Louixie@users.noreply.github.com>
2022-03-10 09:24:06 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("hehe uwu owo :D")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.EigthNote)
+    music.playMelody("C C C D D E E F ", 120)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
